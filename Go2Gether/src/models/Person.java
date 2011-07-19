e01c7283c1d0773548fe8b04d69cecfe9d6c232f
KristoferH <kristoferholmgren@hotmail.com>
2011-07-19 19:02:41 +0200

    Börjat på tester i Personklassen för att se om man kan hämta alla personens resor.

--- Go2Gether/src/models/Person.java
+++ Go2Gether/src/models/Person.java
@@ -12,6 +12,7 @@ public class Person {
 	private String name;
 	private String phoneNumber;
 	private String age;
+	private Journey[] Journeys; 	
 	
 	public String getName(){
 		return name;
@@ -25,6 +26,18 @@ public class Person {
 		return age;
 	}
 	
+	public void addJourney(Journey j){
+		for(int i = 0; i < Journeys.length; i++){
+			if(Journeys[i].equals(null)){
+				
+			}
+		}
+	}
+	
+	public Journey[] getAllJourneys(){
+		return Journeys;
+	}
+	
 	public void setName(String name){
 		this.name = name;
 	}
@@ -36,5 +49,6 @@ public class Person {
 	public void setAge(String age){
 		this.age = age;
 	}
+	
 
 }
--- Go2Gether/src/test/PersonTest.java
+++ Go2Gether/src/test/PersonTest.java
@@ -5,6 +5,8 @@ package test;
 
 import static org.junit.Assert.*;
 
+import models.City;
+import models.Journey;
 import models.Person;
 
 import org.junit.Test;
@@ -28,6 +30,20 @@ public class PersonTest {
 		assertEquals("Testperson", p1.getName());
 		assertEquals("032110622", p1.getPhoneNumber());
 		assertEquals("23", p1.getAge());
+	}
+	
+	public void personKnowsItsJourneys(){
+		
+		Person p1 = new Person();
+		p1.setName("Testperson");
+		p1.setPhoneNumber("032110622");
+		p1.setAge("23");
+		
+		City t1 = new City("Göteborg");
+		City t2 = new City("Stockholm");
+		
+		Journey j = new Journey(t1, t2);
+		
 	}
 
 }
