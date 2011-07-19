6b38407a8bb2eaded9e339b48b394e220c93d3e2
P-O <per-olof.edqvst@tele2.se>
2011-07-19 19:10:06 +0200

    Added Member-Tests, Next change Person and MeMber to Traveler

---
+++ Go2Gether/src/test/MemberTests.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+/**
+ * @author P-O Edqvist
+ *
+ */
+public class MemberTests {
+
+	@Test
+	public void memberKnowsItsAttributes(){
+		
+	}
+	
+	@Test
+	public void memberCanBeRegistered(){
+		
+	}
+	
+	@Test
+	public void memberCanBeUnregistered(){
+		
+	}
+	
+	@Test
+	public void memberCanChangeAttributes(){
+		
+	}
+	
+	@Test
+	public void memberCanExportToPersistantStorage(){
+		
+	}
+	
+	@Test
+	public void memberCanImportFromPersistantStorage(){
+		
+	}
+	
+}
