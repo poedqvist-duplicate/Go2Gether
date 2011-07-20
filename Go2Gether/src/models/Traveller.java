360ae4134d7643adba733775ea3e86203d9d5827
KristoferH <kristoferholmgren@hotmail.com>
2011-07-20 22:35:26 +0200

    Removed the classes person and testperson. Added classes Traveller and TravellerTest that are replacement classes for the former classes.

--- Go2Gether/src/models/Traveller.java
+++ Go2Gether/src/models/Traveller.java
@@ -7,7 +7,7 @@ package models;
  * @author Kristofer
  *
  */
-public class Person {
+public class Traveller {
 	
 	private String name;
 	private String phoneNumber;
@@ -26,11 +26,11 @@ public class Person {
 		return age;
 	}
 	
-	//Metod ej klar än
+	//Something goes wrong when i try to set Journey[i] = j
 	public void addJourney(Journey j){
-		for(int i = 0; i < Journeys.length; i++){
-			if(Journeys[i].equals(null)){
-				
+		for(int i = 0; i <= Journeys.length; i++){
+			if(Journeys[i] == null){
+				Journeys[i] = j;
 			}
 		}
 	}
@@ -50,6 +50,5 @@ public class Person {
 	public void setAge(String age){
 		this.age = age;
 	}
-	
 
 }
--- Go2Gether/src/test/JourneyTest.java
+++ Go2Gether/src/test/JourneyTest.java
@@ -6,7 +6,7 @@ package test;
 import static org.junit.Assert.*;
 
 import models.Journey;
-import models.Person;
+import models.Traveller;
 import models.City;
 
 import org.junit.Test;
@@ -19,19 +19,26 @@ public class JourneyTest {
 	@Test
 	public void journeyKnowsItsAttributes() {
 		
+		//Creates cityobjects
 		City t1 = new City("Göteborg");
 		City t2 = new City("Stockholm");
 		City t3 = new City("Ulricehamn");
 		
+		//Creates a new journey with a start- and endpoint
 		Journey j = new Journey(t1, t2);
 		
+		//Checks so that stockholm isn't the starting point for the journey
 		assertFalse(j.getStartCity().equals("Stockholm"));
+		//Checks so that göteborg is the starting point for the journey
 		assertEquals("Göteborg", j.getStartCity());
+		//Checks so that stockholm is the enddestination
 		assertEquals("Stockholm", j.getEndCity());
 		
+		//Changes start and endcity
 		j.setStartCity(t3);
 		j.setEndCity(t1);
 		
+		//Asserts that ulricehamn is the startcity and göteborg the endcity
 		assertEquals("Ulricehamn", j.getStartCity());
 		assertEquals("Göteborg", j.getEndCity());
 	}
