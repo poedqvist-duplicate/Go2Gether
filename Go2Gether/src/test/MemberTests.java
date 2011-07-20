2582f1e25dd6635bfa968d31075b769efe4b3734
P-O <per-olof.edqvst@tele2.se>
2011-07-20 07:19:23 +0200

    Lagt till sqlitejdbc...jar-filen till build-path. Nu borde vi kunna implementera db-lagret..

--- Go2Gether/src/test/MemberTests.java
+++ Go2Gether/src/test/MemberTests.java
@@ -35,12 +35,36 @@ public class MemberTests {
 	
 	@Test
 	public void memberCanExportToPersistantStorage(){
+		Member m = new Member();
 		
+		assertTrue(m.save());
 	}
 	
 	@Test
 	public void memberCanImportFromPersistantStorage(){
-		
+		int memberId = 346;
+		Member m = new Member(memberId);
 	}
 	
+	
+	
+	/*
+	 * Member
+	 * Name   | Phone   | Id
+	 * P-O    | 1234    | 1
+	 */
+	
+	//P-O#1234#1
+	//Member#Name#Phone#Id
+	public boolean saveMe(String s, String table){
+		//öppna connection
+		//Skapa sql-sträng av infomationen i s..
+		// skicka sql-sträng till db-motor
+		// ta emot resultat
+		//beräkna hur många rader som borde ändras
+		//kontrollera om antalet rader som faktiskt ändrades var så många och returnera det..
+		int numRowsAffected = 1;
+		int actually = db.executeSQL();
+		return actually == numRowsAffected;
+	}
 }
